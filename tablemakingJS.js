55bfbb3824cd8b55cbad37d9a302073ada48a478
Keitaro Sonoda <sonokeihty@gmail.com>
2025-02-04 21:31:09 +0900

    be getting rid of thFloor

--- tablemakingJS.js
+++ tablemakingJS.js
@@ -1,35 +1,37 @@
 floorTable = document.getElementById('floorTable');
-const makeTh = document.createElement('th');
-const makeTd = document.createElement('td');
 
 sixthFloor = document.getElementById('sixthFloor');
+const makeTh = document.createElement('th');
 const makeText = document.createTextNode(floorNames[0]);
 sixthFloor.appendChild(makeTh).appendChild(makeText);
 
-wingNames.reverse().forEach(element => {
+[...wingNames].reverse().forEach(element => {
     const makeTd = document.createElement('td');
     const makeTdText = document.createTextNode(sortedRooms[0][floorNames[0]][element].join(' '));
     makeTd.appendChild(makeTdText);
     sixthFloor.appendChild(makeTd);
 });
 
-document.getElementById('put5thFloor').innerHTML = floorNames[1];
+fifthFloor = document.getElementById('fifthFloor');
+const makeTh2 = document.createElement('th');
+const makeText2 = document.createTextNode(floorNames[1]);
+fifthFloor.appendChild(makeTh2).appendChild(makeText2);
 
-const tableData5 = sortedRooms[0][floorNames[1]][wingNames[2]].join(' '); // Extract room numbers and join them with a comma
-document.getElementById('tableData51').innerHTML = tableData5;
-
-const tableData52 = sortedRooms[0][floorNames[1]][wingNames[1]].join(' ');
-document.getElementById('tableData52').innerHTML = tableData52;
-
-const tableData53 = sortedRooms[0][floorNames[1]][wingNames[0]].join(' ');
-document.getElementById('tableData53').innerHTML = tableData53;
-
-
-
-// practice
-
-let nums = [43, 45, 47, 49].reverse();
-nums.forEach((n,i,nums) => {
-    console.log(n,i,nums)
+[...wingNames].reverse().forEach(element => {
+    const makeTd2 = document.createElement('td');
+    const makeTdText2 = document.createTextNode(sortedRooms[0][floorNames[1]][element].join(' '));
+    makeTd2.appendChild(makeTdText2);
+    fifthFloor.appendChild(makeTd2);
 });
 
+fourthFloor = document.getElementById('fourthFloor');
+const makeTh3 = document.createElement('th');
+const makeText3 = document.createTextNode(floorNames[2]);
+fourthFloor.appendChild(makeTh3).appendChild(makeText3);
+
+[...wingNames].reverse().forEach(element => {
+    const makeTd3 = document.createElement('td');
+    const makeTdText3 = document.createTextNode(sortedRooms[0][floorNames[2]][element].join(' '));
+    makeTd3.appendChild(makeTdText3);
+    fourthFloor.appendChild(makeTd3);
+});
