c7d05f040d3ff29170e0f62d0360bf81dc9029b0
Keitaro Sonoda <sonokeihty@gmail.com>
2025-02-11 19:24:39 +0900

    be organize code

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-import tableMake from "./tablemakingJS.js";
+import { tableMake, tableMake19} from "./tablemakingJS.js";
 
 const allRoomsAndTime = [[307,"19:00"], [505,"18:00"],[623, "18:00"], [618,"18:00"], [608,"18:00"], [625, "18:00"], [203, "18:00"], [303, "18:00"], [423, "19:00"], [424, "18:00"],[502, "19:00"],[418, "18:00"], [408, "18:00"],[621,"18:00"]];
 const floorNames = ['6th floor', '5th floor', '4th floor', '3rd floor', '2nd floor'];
 const wingNames = ['wing 1', 'wing 2', 'wing 3'];
 
-tableMake(floorNames, wingNames, allRoomsAndTime);
+document.getElementById("table18:00").addEventListener('click', () => tableMake(floorNames, wingNames, allRoomsAndTime));
+document.getElementById("table19:00").addEventListener('click', () => tableMake19(floorNames, wingNames, allRoomsAndTime));
 
 document.getElementById("book18:00").addEventListener('click', () => {
     allRoomsAndTime.push([parseInt(document.getElementById("roomNumber").value), "18:00"]);
--- tablemakingJS.js
+++ tablemakingJS.js
@@ -1,11 +1,9 @@
-export default function tableMake(floorNames, wingNames, allRoomsAndTime) {
-    const dinnerTime = ['18:00', '19:00'];
-    const sortedRoomsbytime = [
-        [...allRoomsAndTime].filter(room => room[1] === dinnerTime[0]).map(room => room[0]),
-        [...allRoomsAndTime].filter(room => room[1] === dinnerTime[1]).map(room => room[0])
-    ];
-    
-    const sortedRooms = sortedRoomsbytime.map(timeGroup => 
+const dinnerTime = ['18:00', '19:00'];
+
+export function tableMake(floorNames, wingNames, allRoomsAndTime) {
+    const sortedRooms = dinnerTime.map(time =>
+        [...allRoomsAndTime].filter(room => room[1] === time).map(room => room[0])
+    ).map(timeGroup => 
         Object.fromEntries(
             floorNames.map((floor, index) => [
                     floor,
@@ -26,6 +24,37 @@ export default function tableMake(floorNames, wingNames, allRoomsAndTime) {
 
         [...wingNames].reverse().forEach(element => {
             floor.insertCell().textContent = sortedRooms[0][floorName][element].join(' ');
+        });
+    });
+
+}
+
+export function tableMake19(floorNames, wingNames, allRoomsAndTime) {
+    const dinnerTime = ['18:00', '19:00'];
+
+    const sortedRooms = dinnerTime.map(time =>
+        [...allRoomsAndTime].filter(room => room[1] === time).map(room => room[0])
+    ).map(timeGroup => 
+        Object.fromEntries(
+            floorNames.map((floor, index) => [
+                    floor,
+                    Object.fromEntries(
+                        wingNames.map((wing, wingIndex) => 
+                            [wing, timeGroup.filter(x => x >= (6 - index) * 100 + 10 * wingIndex && x < (6 - index) * 100 + 10 * (wingIndex + 1)).sort((a, b) => b - a)]
+                        )
+                    )])
+        )
+    );
+    console.log(sortedRooms)
+    
+    const floorTable = document.getElementById('floorTable').appendChild(document.createElement('tBody'));
+    floorNames.forEach((floorName) => {
+        const floor = floorTable.insertRow();
+        floor.appendChild(document.createElement('th'))
+            .appendChild(document.createTextNode(floorName));
+
+        [...wingNames].reverse().forEach(element => {
+            floor.insertCell().textContent = sortedRooms[1][floorName][element].join(' ');
         });
     });
 }
