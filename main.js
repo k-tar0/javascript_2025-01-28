e70e8c2fed067875e28145b90493e1f1e8dd73d3
Keitaro Sonoda <sonokeihty@gmail.com>
2025-02-27 09:13:55 +0900

    translated it

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import { makeTableHead, sortRooms, roomsToTable, makeForm } from "./tablemakingJS.js";
 
 const allRoomsAndTime = [[307,"19:00"], [505,"18:00"],[623, "18:00"], [618,"18:00"], [608,"18:00"], [625, "18:00"], [203, "18:00"], [303, "18:00"], [423, "19:00"], [424, "18:00"],[502, "19:00"],[418, "18:00"], [408, "18:00"],[621,"18:00"]];
-const floorNames = ['6th floor', '5th floor', '4th floor', '3rd floor', '2nd floor'];
+const floorNames = ['6F', '5F', '4F', '3F', '2F'];
 const wingNames = ['wing 1', 'wing 2', 'wing 3'];
-const dinnerTime = ['18:00', '19:00'];
-const thead = [' ' , "wing 3", "wing 2", "wing 1"];
+const dinnerTime = ['18:00','18:30','19:00','19:30'];
+const thead = [' ' , "3号館", "2号館", "1号館"];
 
 makeTableHead(thead);
 makeForm(dinnerTime);
@@ -27,5 +27,8 @@ dinnerTime.forEach((time, index) => {
         allRoomsAndTime.push([parseInt(document.getElementById("roomNumber").value), time]);
         sortedRooms = sortRooms(floorNames, wingNames, allRoomsAndTime, dinnerTime);
         roomsToTable(floorNames, wingNames, sortedRooms, dinnerTime, index);
+        dinnerTime.forEach(time => document.getElementById(`table${time}`).style.backgroundColor = "");
+        document.getElementById(`table${time}`).style.backgroundColor = "tomato";
+        document.getElementById("roomNumber").value = "";
     });
 });
