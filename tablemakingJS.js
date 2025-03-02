9ab82b9ca5b3bd191c07fe5ce1a1f205638bab6b
Keitaro Sonoda <sonokeihty@gmail.com>
2025-03-02 13:28:17 +0900

    be making lines

--- tablemakingJS.js
+++ tablemakingJS.js
@@ -42,32 +42,25 @@ export const makeForm = (dinnerTime) => {
     const form = document.createElement("form");
     form.id = "form";
 
-    const createButton = (id, text) => {
-        const button = document.createElement("button");
-        button.type = "button";
-        button.id = id;
-        button.textContent = text;
-        return button;
-    };
+    const createButton = (id, text) => 
+        Object.assign(document.createElement("button"), 
+    { type: "button", id, textContent: text });
 
     dinnerTime.forEach(time => form.appendChild(createButton(`table${time}`, time)));
 
-    form.appendChild(document.createElement("br"));
-    form.appendChild(document.createElement("br"));
+    form.append(document.createElement("br"), document.createElement("br"));
 
     form.appendChild(Object.assign(document.createElement("label"), { textContent: "部屋番号を入力:" }));
     form.appendChild(Object.assign(document.createElement("input"), { id: "roomNumber", type: "number", placeholder: "例: 302", style: "width: 100px;" }));
     form.appendChild(Object.assign(document.createElement("input"), { type: "reset" }));
 
-    form.appendChild(document.createElement("br"));
-    form.appendChild(document.createElement("br"));
+    form.append(document.createElement("br"), document.createElement("br"));
 
     form.appendChild(Object.assign(document.createElement("label"), { textContent: "夕食の時間を入力:" }));
 
     dinnerTime.forEach(time => form.appendChild(createButton(`book${time}`, time)));
-
-    form.appendChild(document.createElement("br"));
-    form.appendChild(document.createElement("br"));
+    
+    form.append(document.createElement("br"), document.createElement("br"));
 
     document.body.appendChild(form);
 };
