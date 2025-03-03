9047bd5d38d0de5c034b948313ca20fb718c0c92
Keitaro Sonoda <sonokeihty@gmail.com>
2025-03-03 20:21:26 +0900

    input submitted automatically

--- formMaking.js
+++ formMaking.js
@@ -14,13 +14,7 @@ export const makeForm = (dinnerTime) => {
     form.appendChild(Object.assign(document.createElement("input"), { type: "text", id: "numInput", maxLength: 3, placeholder: "Enter 3 digits" }));
 
     form.append(document.createElement("br"), document.createElement("br"));
-
-    form.appendChild(Object.assign(document.createElement("label"), { textContent: "夕食の時間を入力:" }));
-
-    dinnerTime.forEach(time => form.appendChild(createButton(`book${time}`, time)));
     
-    form.append(document.createElement("br"), document.createElement("br"));
-
     form.appendChild(createButton("calculate", "ルートの計算"));
 
     document.body.appendChild(form);
