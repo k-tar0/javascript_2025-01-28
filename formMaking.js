ec512aec0eb9c631f468c28c84cbd642d23e300f
Keitaro Sonoda <sonokeihty@gmail.com>
2025-03-03 21:02:27 +0900

    changed time button

--- formMaking.js
+++ formMaking.js
@@ -6,7 +6,9 @@ export const makeForm = (dinnerTime) => {
         Object.assign(document.createElement("button"), 
     { type: "button", id, textContent: text });
 
-    dinnerTime.forEach(time => form.appendChild(createButton(`table${time}`, time)));
+    const div = Object.assign(document.createElement("div"), { className: "buttonGroup" });
+    dinnerTime.forEach(time => div.appendChild(createButton(`table${time}`, time)));
+    form.appendChild(div);
 
     form.append(document.createElement("br"), document.createElement("br"));
 
