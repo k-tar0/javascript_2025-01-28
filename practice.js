b72dd65909db1915e93acf0cbed1b12af3cefc67
Keitaro Sonoda <sonokeihty@gmail.com>
2025-03-06 08:50:52 +0900

    practice COR error: 0.0

--- practice.js
+++ practice.js
@@ -1,11 +1,43 @@
-const bombedTarget = [ , ,
-    "hospitals",
-    "schools",
-    "mosques",
-    "churches",
-];
 
-const result = bombedTarget.map((target) =>
-     `The ${target} were bombed`);
+// const input = document.querySelector('input');
+//       h2 = document.querySelector('h2');
+// h2.innerHTML = localStorage.getItem('value');
+// input.addEventListener("keyup", display);
+// function display() {
+//     localStorage.setItem('value', input.value);
+//     h2.innerHTML = localStorage.getItem('value');
+// }
 
-console.log(bombedTarget[0]);
+    const express = require('express');
+    const app = express();
+    app.use(cors({
+        origin: '*'
+    }))
+    app.use(express.json());
+    
+    let data = [];
+    
+    app.get('/data', (req, res) => res.json(data));
+    app.post('/data', (req, res) => {
+        data.push(req.body.data);
+        res.json({ message: "Data added!", data });
+    });
+    app.listen(3000, () => console.log('Server running on port 3000'));
+    
+// let num = localStorage.getItem("num") || 0; // Get saved num or default to 0
+// num++; 
+// localStorage.setItem("num", num); // Save it
+// document.body.innerHTML = `<h1>Num: ${num}</h1>`;
+// console.log(num);
+
+// document.getElementById("numInput").addEventListener("input", function () {
+//     let value = this.value;
+//     if (value.length === 3) {
+//         myFunction(value);
+//     }
+// });
+
+// function myFunction(val) {
+//     alert("You entered: " + val);
+//     localStorage.setItem("numInput", val);
+// }
