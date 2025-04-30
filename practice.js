0a70a72450d293c51841e7dbdf87121ea1286356
Keitaro Sonoda <sonokeihty@gmail.com>
2025-04-30 21:39:40 +0900

    Add input field and save data

--- practice.js
+++ practice.js
@@ -5,3 +5,17 @@
     app.get('/', (req, res) => 
         res.sendFile(path.join(__dirname, 'practice.html')        ));
     app.listen(3000, () => console.log('Server running on port 3000'));
+
+    app.use(express.json());
+
+    let savedData = "";
+    app.post('/save-data', (req, res) => {
+        savedData = req.body.data;
+        console.log('Data saved:', savedData);
+
+        res.status(200).send('Data saved successfully!');
+    });
+
+    document.getElementById('dataForm').addEventListener('submit', function (e) {
+        e.preventDefault(); 
+        const data = document.getElementById('sharedData').value;
