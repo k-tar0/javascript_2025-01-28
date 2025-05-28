eb62c7adf6fbef01d0ab870ce8e360743ca504e7
Keitaro Sonoda <sonokeihty@gmail.com>
2025-05-28 14:38:14 +0900

    practiced Nodejs

--- practice.js
+++ practice.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json()); 
-    
-app.post('/save-data', (req, res) => {
-    console.log('Data received:', req.body.data);
-    res.send('Data saved successfully!');
+const fs = require('fs');
+if (!fs.existsSync('./newDir')) {
+    fs.mkdir('newDir', (err) => {
+    if (err) {
+        console.error('Error creating directory:', err);
+    } 
+    console.log('Directory created successfully');
 });
-    
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+} else {
+    fs.rmdir('newDir', (err)=> {
+    if (err) {
+        console.error('Error removing directory:', err);
+    } 
+    console.log('Directory removed successfully');
+})};
